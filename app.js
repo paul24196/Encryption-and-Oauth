eea48e3bbe2ad13ce3b11a743a1751a84f866b53
Sam Paul <samuraiben10000@gmail.com>
2021-12-19 11:44:07 +0530

    env commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const schema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+schema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
+
+const User = mongoose.model("user", schema);
+
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+
+app.post("/register", function(req,res){
+  const newuser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newuser.save(function(err){
+    if(err){
+      console.log(err);
+    }else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req,res){
+  const email = req.body.username;
+  const password = req.body.password;
+  User.findOne({email:email}, function(err,result){
+    if(err){
+      console.log(err);
+    }else {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server is online");
+});
